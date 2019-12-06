38af29c9c0fbf4fa3c7d23a477c724c65eb5a953
Иван Иван <remillius_dies@outlook.com>
2019-12-07 01:09:32 +0300

    Add project files.

---
+++ dotNETtask8/Inverser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotNETtask8
+{
+    class Inverser
+    {
+        public delegate double RealFunc(double x);
+
+        public Action<double> PrecisionTracker ;
+
+        public double Inverse(double a, double b, RealFunc f, double y, double epsilon)
+        {
+            double x = (b - a) / 2 + a;
+            double fValue = f(x);
+            while (Math.Abs(fValue-y)>epsilon)
+            {
+                
+                if (fValue>y)
+                {
+                    b = x;
+                    x = (b - a) / 2 + a;
+                    fValue = f(x);
+                }
+
+                if (fValue < y)
+                {
+                    a = x;
+                    x = (b - a) / 2 + a;
+                    fValue = f(x);
+                }
+                Thread.Sleep(400);
+                PrecisionTracker?.Invoke(Math.Abs(fValue - y));
+
+                //Console.WriteLine($"a={a}; b={b}; x ={x}; f(x)={fValue}; y={y}");
+            }
+            return x;
+        }
+    }
+}
---
+++ dotNETtask8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotNETtask8
+{
+    class Program
+    {
+        static private double epsilon = 1e-4;
+
+        static void Main(string[] args)
+        {
+            Inverser inverser = new Inverser();
+            inverser.PrecisionTracker += ShowProgress;
+            Console.WriteLine("Находим значения х, при которых: \n");
+
+            Console.WriteLine($"1.  0.5 = sin(x) на отрезке [0.1, 1.3]");
+            Console.WriteLine($" Ответ: x = {inverser.Inverse(0.1, 1.3, Math.Sin, 0.5, epsilon)}\n");
+
+            Console.WriteLine($"2.  8 = x^2 + sin(x-2) на отрезке [2.5, 3.5]");
+            Console.WriteLine($" Ответ: x = {inverser.Inverse(2.5, 3.5, x => x * x + Math.Sin(x - 2), 8, epsilon)}\n");
+
+            Inverser.RealFunc myFunction = Sigmoida;
+            Console.WriteLine($"3.  0.5 = Сигмоида(x) на отрезке [-4, 5]");
+            Console.WriteLine($" Ответ: x = {inverser.Inverse(-4, 5, myFunction, 0.5, epsilon)}\n");
+            
+            Console.ReadLine();
+        }
+
+        static void ShowProgress(double current)
+        {
+            if (current > epsilon)
+                Console.Write(
+                    $"\r{Math.Log10(current) / Math.Log10(epsilon) * 100:0.}% (текущая точность: 10^{Math.Log10(current):0.})");
+            else
+                Console.WriteLine($"\r {100}% (текущая точность: 10^{Math.Log10(current):0.})");
+        }
+
+        static double Sigmoida(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+    }
+
+}
